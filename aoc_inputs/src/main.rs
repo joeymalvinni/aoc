55b378b18925ad45e3129d56399e58d0bfd6428b
Joey Malvinni <76851062+joeymalvinni@users.noreply.github.com>
2023-12-13 15:21:35 -0800

    add claop

--- aoc_inputs/src/main.rs
+++ aoc_inputs/src/main.rs
@@ -2,11 +2,21 @@ use reqwest;
 use chrono::{DateTime, FixedOffset, Utc, Datelike};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use clap::Parser;
 
 const LAST_YEAR: i32 = 2023;
 const SESSION: &str = "YOUR SESSION COOKIE";
-
 const FILE_PATH: &str = "./in";
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    year: Option<u16>,
+
+    #[arg(short, long)]
+    day: Option<u8>,
+}
 
 fn get_date_info() -> (u32, i32) {
 	let curr: DateTime<Utc> = Utc::now();
@@ -28,15 +38,16 @@ async fn make_request(url: &str) -> Result<(), Box<dyn std::error::Error>> {
 	let client = reqwest::Client::new();
     let response = client.get(url)
         .header("Cookie", format!("session={}", SESSION))
+		.header("User-Agent", "https://github.com/joeymalvinni/aoc/aoc_inputs")
         .send()
         .await?;
-
+	
 	if response.status().is_success() {
 		let body_bytes = response.bytes().await?;
-
+    	
 		let mut file = File::create(FILE_PATH).await?;
 		file.write_all(&body_bytes).await?;
-
+        
 		Ok(())
     } else if response.status() == reqwest::StatusCode::NOT_FOUND {
 		eprintln!("Error: Received a 404 Not Found response.");
@@ -49,11 +60,29 @@ async fn make_request(url: &str) -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let (day, year) = get_date_info();
+	let args = Args::parse();
+	
+	if SESSION == "YOUR SESSION COOKIE" {
+		panic!("Set a valid session cookie");
+	}
 
+	let (mut day, mut year) = get_date_info();
+	
+	if let Some(y) = args.year {
+		if y >= 2015 && y <= 2023 {
+			year = i32::from(y);
+		}
+	}	
+
+	if let Some(d) = args.day {
+		if d >= 1 && d <= 35 {
+			day = u32::from(d);
+		}
+	}
+	
 	let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
 
-  make_request(&url).await?;
+    make_request(&url).await?;
 
 	Ok(())
 }
